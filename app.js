81fd2e533d992113e8632da09566b02d1d002a50
sudhanshu-bh <bsudhanshu3@gmail.com>
2022-10-05 17:34:23 +0530

    env and mongodb model(schema) setup

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema.js';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
 
 const app = express();
+config();
+
+const PORT = process.env.PORT || 4000;
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+mongoose.connection.once('open', () => {
+  console.log('Connected to the database...');
+});
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
-const port = 4000;
-
-app.listen(port, () => {
-  console.log(`Now listening for requests on port ${port}...`);
+app.listen(PORT, () => {
+  console.log(`Now listening for requests on port ${PORT}...`);
 });
